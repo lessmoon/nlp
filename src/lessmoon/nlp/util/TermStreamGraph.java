4e9959b1df75f9cdd13805458230ebc86c87cbf5
lsmcsp@gmail.com <lsmcsp@gmail.com>
2014-10-16 22:47:09 +0800

    Finished the algorithms but not tested yet

--- src/lessmoon/nlp/util/TermStreamGraph.java
+++ src/lessmoon/nlp/util/TermStreamGraph.java
@@ -3,10 +3,10 @@ package lessmoon.nlp.util;
 import java.util.*;
 
 class TermGraphNode {
-    TermGraphNode  nextnode;
-    int            weight;
-    DataNode       term;             
-    boolean        isMainPathNode;
+    final int         weight;
+    TermGraphNode     nextnode;
+    final DataNode    term;             
+    boolean           isMainPathNode;
 
 
     public TermGraphNode(DataNode t,int w,TermGraphNode next){
@@ -27,6 +27,10 @@ class TermGraphNode {
         return nextnode;
     }
 
+    public int getWeight(){
+        return weight;
+    }
+    
     public ArrayList<TermGraphNode> getNextNodeList(){
         ArrayList<TermGraphNode> res = new ArrayList<TermGraphNode>();
         res.add(nextnode);
@@ -37,8 +41,16 @@ class TermGraphNode {
         nextnode = next;
     }
 
+    public DataNode getTerm(){
+        return term;
+    }
+    
     public boolean hasNext(){
         return nextnode != null;
+    }
+    
+    public String toString(){
+        return term.toString() + nextnode.toString();
     }
 }
 
@@ -61,6 +73,74 @@ class MainPathNode extends TermGraphNode{
 
     public ArrayList<TermGraphNode> getNextNodeList(){
         return nextlist;
+    }
+}
+
+class MaxPathGenerator {
+    Map<PathNode,Integer> pimap = new HashMap<PathNode,Integer>();
+    ArrayList<PathNode> ipmap = new ArrayList<PathNode>();
+    int[] pweights;
+    
+    MaxPathGenerator(){
+        
+    }
+    
+    int countNumber(PathNode s,PathNode d){
+        parseGraph(s,p);/*Give each node a number to identify them*/
+        return pimap.size();
+    }
+
+    void parseGraph(PathNode s,PathNode d){
+        pimap.put(s,pimap.size() - 1);
+        ipmap.add(s);
+        if(s == d){
+            return;
+        } else {
+            for(TermGraphNode e : s.getNextNodeList()){
+                if(pimap.get(e) == null){ 
+                    parseGraph(e,d);
+                }
+            }
+        }
+    }
+    
+    public TermGraphNode maxPath(PathNode s,PathNode d){
+        pweights = new int[countnumber(s,d)];
+        for(int i = 0 ; i < pweights.size();i++){
+            pweights[i] = -1;
+        }
+        int max = -1;
+        TermGraphNode mid = null;
+        for(TermGraphNode e : s.getNextNodeList()){
+            TermGraphNode tmp = getPath();
+            if(max < pweights[pimap.get(e).intValue()]){
+                mid = e;
+            }
+        }
+        return new TermGraphNode(ipmap[idx_s].getTerm(),s.getWeight(),mid);
+    }
+
+    TermGraphNode getPath(PathNode s,PathNode d){
+        if(s == d){
+            pweights[pimap.get(d).intValue()] = d.getWeight();
+            return d;
+        }
+        int max = -1;
+        int idx_s = pimap.get(s).intValue();
+        TermGraphNode mid = null;
+        for(TermGraphNode e : s.getNextNodeList()){
+            int idx = pimap.get(e).intValue();
+            int tmp = pweights[idx];
+            if(tmp < 0){/*It is not a valid path weight value*/
+                ipmap[idx] = getPath(e,d);
+            }
+            if(max < tmp){
+                max = tmp;
+                mid = ipmap[idx];
+            }
+        }
+        pweights[idx_s] = max + s.getWeight();
+        return new TermGraphNode(ipmap[idx_s].getTerm(),s.getWeight(),mid);
     }
 }
 
@@ -103,4 +183,5 @@ public class TermStreamGraph {
     public TermGraphNode getBestPath(){
         
     }
+   
 }
